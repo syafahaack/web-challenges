fd41787fafdb992f5575fa7294bb77eda02da82a
Syafa <syafahaack@gmail.com>
2024-06-25 21:06:07 +0200

    add challenge

---
+++ js-input-and-string/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -0,0 +1,53 @@
+/* Sub-Challenge 1
+Implement the following functionality: The value of the first input field is copied into the second input field on button click */
+
+const firstCopyInput = document.querySelector("[data-js=first-copy-input]");
+const secondCopyInput = document.querySelector("[data-js=second-copy-input]");
+const copyButton = document.querySelector("[data-js=copy-button]");
+
+// --v-- write your code here --v--
+copyButton.addEventListener("click", function () {
+  secondCopyInput.value = firstCopyInput.value;
+});
+// --^-- write your code here --^--
+
+/* Sub-Challenge 2
+Implement the following functionality: The value of the first input field is copied in uppercase into the second input field on button click */
+
+const firstCopyInUppercaseInput = document.querySelector(
+  "[data-js=first-copy-uppercase-input]"
+);
+const secondCopyInUppercaseInput = document.querySelector(
+  "[data-js=second-copy-uppercase-input]"
+);
+const copyInUppercaseButton = document.querySelector(
+  "[data-js=copy-uppercase-button]"
+);
+
+// --v-- write your code here --v--
+copyInUppercaseButton.addEventListener("click", function () {
+  secondCopyInUppercaseInput.value =
+    firstCopyInUppercaseInput.value.toUpperCase();
+});
+// --^-- write your code here --^--
+
+/* Sub-Challenge 3
+Implement the following functionality: The values of the two input fields switch on button click */
+
+const firstSwitchValueInput = document.querySelector(
+  "[data-js=first-switch-value-input]"
+);
+const secondSwitchValueInput = document.querySelector(
+  "[data-js=second-switch-value-input]"
+);
+const switchValueButton = document.querySelector(
+  "[data-js=switch-value-button]"
+);
+
+// --v-- write your code here --v--
+switchValueButton.addEventListener("click", function () {
+  let temp = firstSwitchValueInput.value;
+  firstSwitchValueInput.value = secondSwitchValueInput.value;
+  secondSwitchValueInput.value = temp;
+});
+// --^-- write your code here --^--
